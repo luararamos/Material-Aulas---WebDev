1b60a387748162a6ed5e84c830c6d662738ea434
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-25 21:43:27 -0300

    users com api

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Dashboard.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Dashboard.jsx
@@ -1,10 +1,9 @@
 import Tabs from "../components/Tabs";
-
+import { Button } from "@/components/ui/button";
 export default function Dashboard() {
   return (
-    <div className="p-6 flex-1 bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">Bem-vindo ao Dashboard</h1>
-      <Tabs />
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
     </div>
   );
 }
--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
@@ -1,8 +1,106 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { useEffect, useState } from "react"
+import { Button } from "@/components/ui/button";
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+]
+
 export default function Usuarios() {
+
+  const [users, setUsers] = useState([])
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    fetch(`${API_URL}/users`)
+    .then(response => response.json())
+    .then(data => setUsers(data))
+  }, [])
+
+  function handleClick(){
+    alert("Clicou!")
+  }
+
   return (
-    <div className="p-6 flex-1 bg-gray-100">
-      <h1 className="text-2xl font-bold">Usuários</h1>
-      <p className="mt-2 text-gray-600">Página de usuários.</p>
-    </div>
-  );
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">ID</TableHead>
+          <TableHead>Nome</TableHead>
+          <TableHead>e-mail</TableHead>
+          <TableHead className="text-right">Ações</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users.map((pegaItem) => (
+          <TableRow key={pegaItem.id}>
+            <TableCell className="font-medium">{pegaItem.id}</TableCell>
+            <TableCell>{pegaItem.email}</TableCell>
+            <TableCell>{pegaItem.email}</TableCell>
+            <TableCell>
+              <Button onClick = {handleClick}>Editar</Button>
+              <Button>Deletar</Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className="text-right">$2,500.00</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  )
 }
